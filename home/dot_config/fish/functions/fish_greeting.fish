92afae7dc4d8480d4f50186e151f5f7f37a6e723
Barend van der Walt <38956787+bvdwalt@users.noreply.github.com>
2026-05-27 15:20:18 +0200

    fix(fish): scale cowsay width to 65% of terminal columns

--- home/dot_config/fish/functions/fish_greeting.fish
+++ home/dot_config/fish/functions/fish_greeting.fish
@@ -1,8 +1,10 @@
 function fish_greeting
+    set -l cols (tput cols)
+    set -l width (math --scale=0 "$cols * 0.65")
     if command -q cowsay; and command -q fortune
-        fortune | cowsay
+        fortune | cowsay -W $width
     else if command -q cowsay
-        cowsay "Hi 🐟"
+        cowsay -W $width "Hi 🐟"
     else
         echo "Hi 🐟"
     end
